ef54149a7adb96bdb00add381c710611e7534c97
Jefferson Henrique da Silva <jeffersonhsilva01@gmail.com>
2026-05-23 05:40:12 -0300

    Primeiro commit

---
+++ src/main/java/poo/ex_42/Ex_42.java
@@ -0,0 +1,26 @@
+package poo.ex_42;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Jefferson
+ */
+public class Ex_42 {
+
+    public static void main(String[] args) {
+        // Exercício 27 - Lógica de Programação
+        int resposta;
+        do {
+            StringBuilder sb = new StringBuilder("Tabuada\n");
+            int numero = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um número para calcular a tabuada", "Tabuada JAVA", JOptionPane.INFORMATION_MESSAGE));
+            for (int i = 0; i <= 10; i++) {
+                int resultado = numero * i;
+                sb.append(numero).append(" x ").append(i).append(" = ").append(resultado).append("\n");
+            }
+            String resultado = sb.toString();
+            JOptionPane.showMessageDialog(null, resultado, "Tabuada JAVA", JOptionPane.PLAIN_MESSAGE);
+            resposta = JOptionPane.showConfirmDialog(null, "Deseja realizar um novo cálculo?", "Tabuada JAVA", JOptionPane.YES_OPTION);
+        } while (resposta == JOptionPane.YES_OPTION);
+    }
+}
